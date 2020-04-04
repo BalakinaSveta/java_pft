6aa2b7f03efce07cf4d66521810fb643c4ff0e4c
BalakinaSveta <46625789+BalakinaSveta@users.noreply.github.com>
2020-04-04 11:55:27 +0300

    Задание № 4. Исправление: Для хранения данных контакта используется один класс ContactData

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,55 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstname;
+  private final String middlename;
+  private final String lastname;
+  private final String mobile;
+  private final String email;
+  private final String birthday;
+  private final String birthmonth;
+  private final String birthyear;
+
+  public ContactData(String firstname, String middlename, String lastname, String mobile, String email, String day, String month, String year) {
+    this.firstname = firstname;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.mobile = mobile;
+    this.email = email;
+    this.birthday = day;
+    this.birthmonth = month;
+    this.birthyear = year;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getBirthday() {
+    return birthday;
+  }
+
+  public String getBirthmonth() {
+    return birthmonth;
+  }
+
+  public String getBirthyear() {
+    return birthyear;
+  }
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestCreationContact.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestCreationContact.java
@@ -30,7 +30,7 @@ public class TestCreationContact {
   public void testCreationContact() throws Exception {
 
     createNewContact();
-    fillContactForm(new FullNameData("Ivan", "Ivanovich", "Ivanov"), new ContactInfo("891655555555", "ivanov@mail.ru"), new DateOfBirth("5", "May", "1975"));
+    fillContactForm(new ContactData("Ivan", "Ivanovich", "Ivanov", "891655555555", "ivanov@mail.ru", "5", "May", "1975"));
     submitContactCreation();
     returnToHomePage();
   }
@@ -39,32 +39,31 @@ public class TestCreationContact {
     wd.findElement(By.linkText("add new")).click();
   }
 
-  private void fillContactForm(FullNameData fullNameData, ContactInfo contactInfo, DateOfBirth dateOfBirth) {
+  private void fillContactForm(ContactData contactData) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(fullNameData.getFirstname());
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
     wd.findElement(By.name("middlename")).click();
     wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys(fullNameData.getMiddlename());
+    wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddlename());
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(fullNameData.getLastname());
-    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
     wd.findElement(By.name("mobile")).click();
     wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(contactInfo.getMobile());
+    wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
     wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(contactInfo.getEmail());
+    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
     wd.findElement(By.name("bday")).click();
-    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(dateOfBirth.getDay());
+    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getBirthday());
     wd.findElement(By.xpath("//div[@id='content']/form/select/option[7]")).click();
     wd.findElement(By.name("bmonth")).click();
-    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(dateOfBirth.getMonth());
+    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getBirthmonth());
     wd.findElement(By.xpath("//div[@id='content']/form/select[2]/option[6]")).click();
     wd.findElement(By.name("byear")).click();
     wd.findElement(By.name("byear")).clear();
-    wd.findElement(By.name("byear")).sendKeys(dateOfBirth.getYear());
+    wd.findElement(By.name("byear")).sendKeys(contactData.getBirthyear());
   }
 
   private void submitContactCreation() {
