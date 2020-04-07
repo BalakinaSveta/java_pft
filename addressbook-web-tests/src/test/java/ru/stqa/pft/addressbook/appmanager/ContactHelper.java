de45b307e2e6c37d231f66c057b98c6e9f299804
BalakinaSveta <46625789+BalakinaSveta@users.noreply.github.com>
2020-04-07 11:45:49 +0300

    Тест на удаление контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -4,9 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-public class ContactHelper extends HelperBase{
+public class ContactHelper extends HelperBase {
 
-  public ContactHelper (WebDriver wd){
+  public ContactHelper(WebDriver wd) {
     super(wd);
   }
 
@@ -15,14 +15,14 @@ public class ContactHelper extends HelperBase{
   }
 
   public void fillContactForm(ContactData contactData) {
-    type(By.name("firstname"),(contactData.getFirstname()));
-    type(By.name("middlename"),(contactData.getMiddlename()));
-    type(By.name("lastname"),(contactData.getLastname()));
-    type(By.name("mobile"),(contactData.getMobile()));
-    type(By.name("email"),(contactData.getEmail()));
+    type(By.name("firstname"), (contactData.getFirstname()));
+    type(By.name("middlename"), (contactData.getMiddlename()));
+    type(By.name("lastname"), (contactData.getLastname()));
+    type(By.name("mobile"), (contactData.getMobile()));
+    type(By.name("email"), (contactData.getEmail()));
     select(By.name("bday"), contactData.getBirthday(), By.xpath("//div[@id='content']/form/select/option[7]"));
     select(By.name("bmonth"), contactData.getBirthmonth(), By.xpath("//div[@id='content']/form/select[2]/option[6]"));
-    type(By.name("byear"),(contactData.getBirthyear()));
+    type(By.name("byear"), (contactData.getBirthyear()));
   }
 
   public void submitContactCreation() {
@@ -30,6 +30,20 @@ public class ContactHelper extends HelperBase{
   }
 
   public void returnToHomePage() {
-    click(By.linkText("home page"));
+    click(By.linkText("home"));
+  }
+
+  public void selectContact() {
+    click(By.name("selected[]"));
+  }
+
+  public void deleteSelectedContact() {
+    click(By.xpath("//input[@value='Delete']"));
+  }
+
+  public void acceptContactDeletion() {
+    wd.switchTo().alert().accept();
   }
 }
+
+
