ec5971fff9cb90ccd5fac22f1f78fc4da0a8353f
BalakinaSveta <46625789+BalakinaSveta@users.noreply.github.com>
2020-04-07 11:46:14 +0300

    Дополнительный коммит

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -6,7 +6,7 @@ import ru.stqa.pft.addressbook.model.ContactData;
 public class ContactCreationTests extends TestBase {
 
   @Test
-  public void testCreationContact() throws Exception {
+  public void testCreationContact() {
 
     app.getContactHelper().initContactCreation();
     app.getContactHelper().fillContactForm(new ContactData("Ivan", "Ivanovich", "Ivanov", "891655555555", "ivanov@mail.ru", "5", "May", "1975"));
