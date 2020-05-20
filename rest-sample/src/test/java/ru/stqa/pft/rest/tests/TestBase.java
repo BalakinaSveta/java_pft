adccbd9bb7faa213e5ce393a7ed5b52e2e48c3e8
BalakinaSveta <46625789+BalakinaSveta@users.noreply.github.com>
2020-05-21 00:59:24 +0300

    Реализована интеграция тестов с баг-трекером Bugify

---
+++ rest-sample/src/test/java/ru/stqa/pft/rest/tests/TestBase.java
@@ -0,0 +1,54 @@
+package ru.stqa.pft.rest.tests;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import com.jayway.restassured.RestAssured;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.message.BasicNameValuePair;
+import org.testng.SkipException;
+import ru.stqa.pft.rest.model.Issue;
+
+import java.io.IOException;
+import java.util.Set;
+
+public class TestBase {
+
+  public Set<Issue> getIssues() throws IOException {
+    String json = getExecutor().execute(Request.Get("https://bugify.stqa.ru/api/issues.json?limit=500"))
+            .returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    JsonElement issues = parsed.getAsJsonObject().get("issues");
+    return new Gson().fromJson(issues, new TypeToken<Set<Issue>>() {
+    }.getType());
+  }
+
+  public Executor getExecutor() {
+    return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
+  }
+
+  public int createIssue(Issue newIssue) throws IOException {
+    String json = getExecutor().execute(Request.Post("https://bugify.stqa.ru/api/issues.json")
+            .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
+                    new BasicNameValuePair("description", newIssue.getDescription())))
+            .returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    return parsed.getAsJsonObject().get("issue_id").getAsInt();
+  }
+
+  public boolean isIssueOpen(int issueId) {
+    String json = RestAssured.get("https://bugify.stqa.ru/api/issues/" + issueId + ".json").asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    String status = parsed.getAsJsonObject().getAsJsonArray("issues").get(0).getAsJsonObject()
+            .get("state_name").getAsString();
+    return status.equals("Open");
+  }
+
+  public void skipIfNotFixed(int issueId) {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
+  }
+}
